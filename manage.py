cc26e0651b2faa03554509078c076a02ba26ecfd
Nick <nickjanquart@gmail.com>
2017-01-26 11:53:24 -0600

    updated passwords in init data

--- manage.py
+++ manage.py
@@ -17,14 +17,14 @@ def run():
 
 @manager.command
 def seed():
-	nick = Eater(first_name="Nick", last_name="Janquart", username="random1")
-	gemma = Eater(first_name="Gemma", last_name="Petrie", username="pammeg")
-	jer = Eater(first_name="Jer", last_name="Janquart", username="numbnuts")
-	matt = Eater(first_name="Matt", last_name="Obrien", username="matto")
-	rc = Eater(first_name="Ryan", last_name="Cowan", username="rycow")
-	peter = Eater(first_name="Peter", last_name="Compernolle", username="pecomp")
-	karl = Eater(first_name="Karl", last_name="Kell", username="karlk")
-	eric = Eater(first_name="Eric", last_name="Sanders", username="chico")
+	nick = Eater(first_name="Nick", last_name="Janquart", username="random1", password="test")
+	gemma = Eater(first_name="Gemma", last_name="Petrie", username="pammeg",password="test")
+	jer = Eater(first_name="Jer", last_name="Janquart", username="numbnuts", password="test")
+	matt = Eater(first_name="Matt", last_name="Obrien", username="matto", password="test")
+	rc = Eater(first_name="Ryan", last_name="Cowan", username="rycow", password="test")
+	peter = Eater(first_name="Peter", last_name="Compernolle", username="pecomp", password="test")
+	karl = Eater(first_name="Karl", last_name="Kell", username="karlk", password="test")
+	eric = Eater(first_name="Eric", last_name="Sanders", username="chico", password="test")
 
 	session.add_all([nick, gemma, jer, matt, rc, peter, karl, eric])
 	session.commit()
