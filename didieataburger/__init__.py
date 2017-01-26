40864080c4f37ac281b27870608a672b488de26a
Nick <nickjanquart@gmail.com>
2017-01-25 20:48:02 -0600

    update to classes

--- didieataburger/__init__.py
+++ didieataburger/__init__.py
@@ -3,9 +3,8 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+config_path = os.environ.get("CONFIG_PATH", "didieataburger.config.DevelopmentConfig")
 app.config.from_object(config_path)
 
-from . import views
-from . import filters
+from . import controller
 from . import login
