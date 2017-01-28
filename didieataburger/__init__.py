f17f561fb264dfe124644bb94ecd422ea5a3078c
Nick <nickjanquart@gmail.com>
2017-01-28 15:28:18 -0600

    trying to define here

--- didieataburger/__init__.py
+++ didieataburger/__init__.py
@@ -9,3 +9,9 @@ app.config.from_object(config_path)
 from . import controller
 from . import login
 from . import filters
+#remove below if doesn't work
+
+if __name__ == '__main__':
+	app.debug = True
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
