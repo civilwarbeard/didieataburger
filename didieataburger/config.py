6f4a7baef24944135a3d38a496a2f4b5419a2381
Nick <nickjanquart@gmail.com>
2017-01-27 22:41:17 -0600

    sql variables

--- didieataburger/config.py
+++ didieataburger/config.py
@@ -1,6 +1,6 @@
 import os
 
 class DevelopmentConfig(object):
-	app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ubuntu:Cthulhu81@localhost:5432/didieataburger"
+	SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:Cthulhu81@localhost:5432/didieataburger"
 	DEBUG = True
 	SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
