6baa0e68893ae49900c8f92b709beb7e11debbe3
Nick <nickjanquart@gmail.com>
2017-01-28 16:34:51 -0600

    updated port again

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ manager = Manager(app)
 
 @manager.command
 def run():
-	port = int(os.environ.get('PORT', 33507))
+	port = int(os.environ.get('PORT', 29865))
 	app.run(host='0.0.0.0', port=port)
 
 @manager.command
