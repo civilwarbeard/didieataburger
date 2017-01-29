ace4812219d4eed05e0b9b03d274b21d2d6a4aa2
Nick <nickjanquart@gmail.com>
2017-01-28 20:07:43 -0600

    updated main

--- manage.py
+++ manage.py
@@ -30,5 +30,9 @@ def seed():
 	session.commit()
 
     
-if __name__ == "__main__":
-    manager.run()
+ if __name__ == '__main__':
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
+
+    
