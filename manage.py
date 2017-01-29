5b41e6b1f8f9bdde71531deebde5783a69597743
Nick <nickjanquart@gmail.com>
2017-01-28 20:09:03 -0600

    take it back

--- manage.py
+++ manage.py
@@ -30,9 +30,7 @@ def seed():
 	session.commit()
 
     
- if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    manager.run()
 
     
