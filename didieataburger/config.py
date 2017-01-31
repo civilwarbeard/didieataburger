39e2b4e834fb1a24b4eff21e18724f0e403474a4
Nick <nickjanquart@gmail.com>
2017-01-30 22:45:07 -0600

    updated pw

--- didieataburger/config.py
+++ didieataburger/config.py
@@ -1,6 +1,6 @@
 import os
 
 class DevelopmentConfig(object):
-	SQLALCHEMY_DATABASE_URI = "postgres://sxarmwdrwkkxns:1b29ad634dac46c7a5246367d32b69884bdb1f31be75c24d2d46d20ede846539@ec2-107-22-236-252.compute-1.amazonaws.com:5432/dbiqc6vqfknu9f"
+	SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:Cthulhu81@localhost:5432/didieataburger"
 	DEBUG = True
 	SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
