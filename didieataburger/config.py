6817a5acb896db17b02dd0bd27c21f83e3629729
Nick <nickjanquart@gmail.com>
2017-01-30 21:06:29 -0600

    trying again

--- didieataburger/config.py
+++ didieataburger/config.py
@@ -1,6 +1,6 @@
 import os
 
 class DevelopmentConfig(object):
-	SQLALCHEMY_DATABASE_URI = "postgres://dycyenkhzpwrpr:4922ccce654d5d79d4e4a264b85ad973f9b4d71c22f619e5668365bad4a9a587@ec2-54-163-224-108.compute-1.amazonaws.com:5432/dac17q38527sv4"
+	SQLALCHEMY_DATABASE_URI = "postgres://sxarmwdrwkkxns:1b29ad634dac46c7a5246367d32b69884bdb1f31be75c24d2d46d20ede846539@ec2-107-22-236-252.compute-1.amazonaws.com:5432/dbiqc6vqfknu9f"
 	DEBUG = True
 	SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
