d3850f28d2999d23936aa55711e326f38743d756
Nick <nickjanquart@gmail.com>
2017-01-31 20:47:25 -0600

    fucking hell

--- didieataburger/controller.py
+++ didieataburger/controller.py
@@ -50,7 +50,7 @@ def eat():
 @login_required
 def eat_post():
 	#enter burger query update
-	burger_eater = current_user
+	burger_eater = current_user.id
 
 	session.add(Burger(eater=burger_eater))
 
