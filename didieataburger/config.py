234b46018cdd71680c24a0128fe6e2eaf1ff4618
Nick <nickjanquart@gmail.com>
2017-02-02 20:46:31 -0600

    totally new approach

--- didieataburger/config.py
+++ didieataburger/config.py
@@ -4,3 +4,8 @@ class DevelopmentConfig(object):
 	SQLALCHEMY_DATABASE_URI = "postgres://sxarmwdrwkkxns:1b29ad634dac46c7a5246367d32b69884bdb1f31be75c24d2d46d20ede846539@ec2-107-22-236-252.compute-1.amazonaws.com:5432/dbiqc6vqfknu9f"
 	DEBUG = True
 	SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
+
+class HerokuConfig(object):
+	SQLALCHEMY_DATABASE_URI = "postgres://sxarmwdrwkkxns:1b29ad634dac46c7a5246367d32b69884bdb1f31be75c24d2d46d20ede846539@ec2-107-22-236-252.compute-1.amazonaws.com:5432/dbiqc6vqfknu9f"
+	DEBUG = True
+	SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
