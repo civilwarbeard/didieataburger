e8d4de3eced3bc9f72e84116cb48db97622da722
Nick <nickjanquart@gmail.com>
2017-02-15 21:22:40 -0600

    updated some stuff

--- didieataburger/login.py
+++ didieataburger/login.py
@@ -7,6 +7,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = "login_get"
+login_manager.login_message_category = "danger"
 
 @login_manager.user_loader
 def load_user(id):
