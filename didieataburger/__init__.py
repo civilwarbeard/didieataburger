4f0c2299b19fa7b9bef99f037b3f99601d0c586b
Nick <nickjanquart@gmail.com>
2017-04-03 19:45:25 -0500

    reset db

--- didieataburger/__init__.py
+++ didieataburger/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-config_path = os.environ.get("CONFIG_PATH", "didieataburger.config.HerokuConfig")
+config_path = os.environ.get("CONFIG_PATH", "didieataburger.config.DevelopmentConfig")
 app.config.from_object(config_path)
 
 from . import controller
